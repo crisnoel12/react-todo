2b5c1277b56f92ad5149e015ef581b75a22e5cb0
Cris Cancino <crisnoel12@gmail.com>
2017-09-08 16:42:31 -0700

    added creation_date key to mock todo data

--- src/mock/todos.js
+++ src/mock/todos.js
@@ -2,17 +2,20 @@ const todosMock = [
     {
         id: 0,
         title: "Learn React JS",
-        status: true
+        status: true,
+        creation_date: new Date(2017, 9, 8, 16, 38, 1)
     },
     {
         id: 1,
         title: "Learn React-Router",
-        status: false
+        status: false,
+        creation_date: new Date(2017, 9, 8, 16, 38, 2)
     },
     {
         id: 2,
         title: "Learn Redux",
-        status: false
+        status: false,
+        creation_date: new Date(2017, 9, 8, 16, 38, 3)
     }
 ]
 
